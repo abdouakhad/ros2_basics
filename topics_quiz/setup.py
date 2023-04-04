9c92dc0020b3218c97dc337a04056f567c1213f8
abdouakhad <akhad0015@gmail.com>
2023-04-04 13:28:57 +0000

    completed topics_quiz

--- topics_quiz/setup.py
+++ topics_quiz/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'topics_quiz'
 
@@ -6,10 +8,12 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[
+   data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'topics_quiz_node = topics_quiz.topics_quiz_node:main'
         ],
     },
 )
